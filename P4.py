82b8156c067a962fc3a135ed41ecdbb39a7c8b2c
OriginalFool <nmrcarroll@gmail.com>
2015-03-27 13:54:05 -0600

    Finished design report, added in bowen's changes

--- P4.py
+++ P4.py
@@ -2,23 +2,30 @@ import sys
 import cx_Oracle
 import P1
 
+
 def violation(conString):
+        con = cx_Oracle.connect(conString);          
+        curs = con.cursor()        
         owner=input('is this driver the primary owner of this vehicle? (y/n)')
         if owner.lower() == 'y':
-                sqlcode=record()
+                sqlcode=record(conString,curs)
                 print(sqlcode)
-                con = cx_Oracle.connect(conString);          
-                curs = con.cursor()
+                #con = cx_Oracle.connect(conString);          
+                #curs = con.cursor()
                 curs.execute(sqlcode)
                 con.commit()
                 curs.close
         elif owner.lower() == 'n':
+                print()
                 print('please enter the information of the primary owner of this vehicle')
-                sqlcode1=record()
+                print()
+                sqlcode1=record(conString,curs)
+                print()
                 print('please enter the information of the driver of this vehicle')
-                sqlcode2=record()
-                con = cx_Oracle.connect(conString);          
-                curs = con.cursor()
+                print()
+                sqlcode2=record(conString,curs)
+                #con = cx_Oracle.connect(conString);          
+                #curs = con.cursor()
                 curs.execute(sqlcode1)
                 curs.execute(sqlcode2)
                 con.commit()
@@ -27,7 +34,7 @@ def violation(conString):
                 print('Invalid input.')
             
 
-def record():
+def record(conString,curs):
         end=False
         while end==False:
                 ticket_no =input("Enter ticket number :")
@@ -35,12 +42,12 @@ def record():
                 exist2 =P1.searchSerial(vehicle_id, curs)
                 if not exist2:
                         print("The vehicle is not registered. Please register first")
-                        P3.regV(conString)                 
+                        P1.regV(conString)                 
                 violator_no = input("Enter violator :")
                 exist1 = P1.searchSin(violator_no, curs)
                 if not exist:
                         print("The person is not registered. Please register first")
-                        P3.regPerson(conString, curs)                     
+                        P1.regPerson(conString)                     
                 office_no = input("Enter officer ID :")
                 vtype = input("Enter violation type:")
                 vdate = input("Enter violation date (DD-Month-YY):")
--- P5.py
+++ P5.py
@@ -3,33 +3,50 @@ import cx_Oracle
 
 def search_option1():
     statement = "SELECT p.name, l.licence_no, p.addr, p.birthday, l.class, dc.description, l.expiring_date FROM people p, drive_licence l, driving_condition dc, restriction r WHERE p.sin = l.sin AND l.licence_no = r.licence_no AND r.r_id = dc.c_id AND "
-    choose=input("Please choose the way you want to search? (1: name. 2:drive licence number)")
-
-    if choose == "1":
-        name=input("Please input driver`s name: ")
-        statement += "p.name = '"+name+"'"
-        return statement
-            
+    while True:
+        print()
+        choose=input("Please choose the way you want to search by? (1: name. 2:drive licence number)")
+        if choose == "1":
+            print()
+            name=input("Please input driver`s name: ")
+            statement += "p.name = '"+name+"'"
+            return statement
         
-    elif choose == "2":
-        number=input("Please input drive licence number: ")
-        statement += "l.licence_no = '"+number+"'"
-        return statement
+        elif choose == "2":
+            print()
+            number=input("Please input drive licence number: ")
+            statement += "l.licence_no = '"+number+"'"
+            return statement
+        else:
+            print()
+            print('Invalid input.')
+            print('Please input valid search option')
+            continue
+        
 
 def search_option2():
     statement="SELECT t.*,tt.fine FROM ticket t,ticket_type tt,drive_licence l WHERE t.vtype=tt.vtype AND "
-    choose=input("Please choose the way you want to search? (1: sin of person. 2:drive licence number)")
-
-    if choose == "1":
-        sin=input("Please input sin of person: ")
-        statement += "l.sin = '"+sin+"'"  
-        return statement
+    while True:
+        print()
+        choose=input("Please choose the way you want to search by? (1: sin of person. 2:drive licence number)")
+        if choose == "1":
+            print()
+            sin=input("Please input sin of person: ")
+            statement += "l.sin = '"+sin+"'"  
+            return statement
         
-    elif choose == "2":
-        number=input("Please input drive licence number: ")
-        statement += "l.licence_no = '"+number+"'"
-        return statement
-    
+        elif choose == "2":
+            print()
+            number=input("Please input drive licence number: ")
+            statement += "l.licence_no = '"+number+"'"
+            return statement
+        else:
+            print()
+            print('Invalid input.')
+            print('Please input valid search option')
+            continue    
+
+
 def search_option3():
     serial = input("Enter vehicle's serial number :")
     statement= "SELECT v.serial_no, count( DISTINCT a.transaction_id ), AVG( a.price ), count( DISTINCT t.ticket_no) FROM vehicle v, auto_sale a, ticket t WHERE t.vehicle_id (+)= v.serial_no AND a.vehicle_id (+)= v.serial_no And v.serial_no='"+serial+"' GROUP BY v.serial_no"
@@ -55,25 +72,57 @@ def search_engine(conString):
             return
         elif user_input == '1':
             sqlcode = search_option1()
+            curs.execute(sqlcode)
+            rows = curs.fetchall()
+            if len(rows) == 0:
+                print("No matches. Please try again")
+            else:
+                for row in rows:
+                    print('the name of driver you searched is:',str(row[0]))
+                    print('the drive licence number of driver you searched is:',str(row[1]))
+                    print('the address of driver you searched is:',str(row[2]))
+                    print('the birthday of driver you searched is:',str(row[3])) 
+                    print('the drive licence class of driver you searched is:',str(row[4])) 
+                    print('the description of driving condition of driver you searched is:',str(row[5])) 
+                    print('the drive licence`s expiring_date of driver you searched is:',str(row[6])) 
+                    print()                        
         elif user_input == '2':
             sqlcode = search_option2()
+            curs.execute(sqlcode)
+            rows = curs.fetchall()
+            print(rows)
+            if len(rows) == 0:
+                print("No matches. Please try again")
+            else:
+                for row in rows:
+                    print('the ticket number of violation is:',str(row[0]))
+                    print('the violator number of driver you searched is:',str(row[1]))
+                    print('the serial number of vehicle is:',str(row[2]))
+                    print('the officer number is:',str(row[3])) 
+                    print('the type of violation is:',str(row[4])) 
+                    print('the date of violation is:',str(row[5]))
+                    print('the place of violation is:',str(row[6])) 
+                    print('the descriptions of violation is:',str(row[7]))
+                    print('the amount of the fine is:',str(row[7])) 
+                    print() 
+                    
         elif user_input == '3':
             sqlcode = search_option3()
+            curs.execute(sqlcode)
+            rows = curs.fetchall()
+            if len(rows) == 0:
+                print("No matches. Please try again")
+            else:
+                for row in rows:
+                    print('the serial number of vehicle you searched is:',str(row[0]))
+                    print('the transaction ID of vehicle you searched is:',str(row[1]))
+                    print('the average price of vehicle you searched is:',str(row[2]))
+                    print('the number of violations of vehicle you searched is:',str(row[3]))
+                
         else:
             print()
             print('Invalid input.')
-            continue
-        #print(sqlcode)
-        curs.execute(sqlcode)
-        rows = curs.fetchall()
-        #print(rows)
-        if len(rows) == 0:
-            print("No matches.")
-        else:    
-            for row in rows:
-                for item in row:
-                    print(item,end=" ")
-                print()            
+            continue         
             
         print('Press <ENTER> to continue')
         input("")
